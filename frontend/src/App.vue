ca7925c0fd40ccf104c60ab21daf970dff047c2c
Carmonn <carmonalcb@gmail.com>
2025-08-12 11:59:15 -0600

    🚀 feat(frontend): agregar lógica para mostrar las rutas

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,15 @@
-<script setup lang="ts">
-const message = "Hello, Hono!";
-</script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to
-    read the documentation
-  </p>
-  <p>{{ message }}</p>
+  <v-app>
+    <v-layout>
+      <v-main>
+        <router-view v-slot="{ Component, route }">
+          <v-fade-transition :hide-on-leave="true">
+            <div :key="route.name || 'default-key'" :class="['pa-2']">
+              <component :is="Component"></component>
+            </div>
+          </v-fade-transition>
+        </router-view>
+      </v-main>
+    </v-layout>
+  </v-app>
 </template>
-
-<style scoped></style>
