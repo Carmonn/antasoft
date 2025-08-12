3df91c9f63052f22e996464d65ca0683b11add5e
Carmonn <carmonalcb@gmail.com>
2025-08-12 13:37:26 -0600

    🚀 feat(frontend): agregar estructura basica de la vista login

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <v-app>
-    <v-layout>
-      <v-main>
-        <router-view v-slot="{ Component, route }">
-          <v-fade-transition :hide-on-leave="true">
-            <div :key="route.name || 'default-key'" :class="['pa-2']">
-              <component :is="Component"></component>
-            </div>
-          </v-fade-transition>
-        </router-view>
-      </v-main>
-    </v-layout>
+    <v-main>
+      <router-view v-slot="{ Component, route }">
+        <v-fade-transition :hide-on-leave="true">
+          <div
+            :key="route.name || 'default-key'"
+            :class="['pa-2']"
+            style="height: 100vh"
+          >
+            <component :is="Component"></component>
+          </div>
+        </v-fade-transition>
+      </router-view>
+    </v-main>
   </v-app>
 </template>
--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,5 +1,44 @@
+<script setup lang="ts">
+import imgSrc from "@/assets/advr.png";
+</script>
+
 <template>
-  <h1>Login</h1>
+  <v-container
+    id="LoginView"
+    class="pa-0 mt-n15 d-flex justify-center align-center"
+  >
+    <v-container max-width="500">
+      <v-img aspect-ratio="16/9" :src="imgSrc"></v-img>
+
+      <v-card class="mx-2 mt-3">
+        <v-form>
+          <v-container>
+            <v-row
+              ><v-col cols="12">
+                <v-text-field label="Email" required></v-text-field> </v-col
+            ></v-row>
+            <v-row
+              ><v-col cols="12">
+                <v-text-field
+                  label="Password"
+                  type="password"
+                  required
+                ></v-text-field> </v-col
+            ></v-row>
+            <v-row
+              ><v-col cols="12">
+                <v-btn type="submit" color="primary" block>Login</v-btn>
+              </v-col></v-row
+            >
+          </v-container>
+        </v-form>
+      </v-card>
+    </v-container>
+  </v-container>
 </template>
 
-<style scoped></style>
+<style scoped>
+#LoginView {
+  height: 100%;
+}
+</style>
