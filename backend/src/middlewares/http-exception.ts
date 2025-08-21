0804ea48fd12361525fc4bdbdfc259c63a02f659
Carmonn <carmonalcb@gmail.com>
2025-08-21 14:30:12 -0600

    🚀 feat(backend): crear la lógica y el flujo de trabajo para el manejo de errores

--- backend/src/middlewares/http-exception.ts
+++ backend/src/middlewares/http-exception.ts
@@ -3,7 +3,7 @@ import type { Context } from "hono";
 import type { ContentfulStatusCode } from "hono/utils/http-status";
 
 type ICause = {
-  code?: string;
+  code?: ErrorHelpCode; // opcional, para errores específicos
   detail?: string;
   help?: string;
 };
@@ -13,7 +13,7 @@ type IMultiCause = ICause | ICause[]; // para múltiples errores
 export function createHttpException(
   status: ContentfulStatusCode | undefined,
   message: string,
-  cause?: IMultiCause,
+  cause: IMultiCause,
 ) {
   // normalizamos siempre a array
   const normalizedCause: ICause[] | undefined = cause
@@ -52,9 +52,32 @@ export const errorHandler = (err: Error, c: Context) => {
       error: {
         status: 500,
         code: "INTERNAL_SERVER_ERROR",
-        message: "Internal Server Error",
+        message: err.message || "Error interno del servidor",
       },
     },
     500,
   );
+};
+
+export enum ErrorHelpCode {
+  LOGIN_CORREO_NO_ENCONTRADO = "LOGIN_CORREO_NO_ENCONTRADO",
+  LOGIN_PASSWORD_INCORRECTO = "LOGIN_PASSWORD_INCORRECTO",
+  NO_AUTORIZADO = "NO_AUTORIZADO",
+}
+export const errorHelpMap: Record<ErrorHelpCode, ICause> = {
+  [ErrorHelpCode.NO_AUTORIZADO]: {
+    code: ErrorHelpCode.NO_AUTORIZADO,
+    detail: "No estás autorizado para acceder a este recurso",
+    help: "Asegúrate de haber iniciado sesión",
+  },
+  [ErrorHelpCode.LOGIN_CORREO_NO_ENCONTRADO]: {
+    code: ErrorHelpCode.LOGIN_CORREO_NO_ENCONTRADO,
+    detail: "El usuario no existe",
+    help: "Verifica que el correo electrónico sea correcto",
+  },
+  [ErrorHelpCode.LOGIN_PASSWORD_INCORRECTO]: {
+    code: ErrorHelpCode.LOGIN_PASSWORD_INCORRECTO,
+    detail: "La contraseña es incorrecta",
+    help: "Verifica que la contraseña sea correcta",
+  },
 };
